c71d224ca2d4ddf11ee6c731756a46d71b6c1cc7
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-01-28 18:15:13 +0000

    bidirectional grpc implementation added and working

---
+++ nick_ch03/bidirectional_streaming_rpc/client/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	pb "client/gen"
+	"context"
+	"io"
+	"log"
+	"time"
+
+	wrapper "github.com/golang/protobuf/ptypes/wrappers"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Setting up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// Process Order : Bi-di streaming scenario
+	streamProcOrder, err := client.ProcessOrders(ctx)
+	if err != nil {
+		log.Fatalf("%v.ProcessOrders(_) = _, %v", client, err)
+	}
+
+	if err := streamProcOrder.Send(&wrapper.StringValue{Value: "102"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "102", err)
+	}
+
+	if err := streamProcOrder.Send(&wrapper.StringValue{Value: "103"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "103", err)
+	}
+
+	if err := streamProcOrder.Send(&wrapper.StringValue{Value: "104"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "104", err)
+	}
+
+	channel := make(chan struct{})
+	go asyncClientBidirectionalRPC(streamProcOrder, channel)
+	time.Sleep(time.Millisecond * 1000)
+
+	if err := streamProcOrder.Send(&wrapper.StringValue{Value: "101"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "101", err)
+	}
+	if err := streamProcOrder.CloseSend(); err != nil {
+		log.Fatal(err)
+	}
+	channel <- struct{}{}
+}
+
+func asyncClientBidirectionalRPC(streamProcOrder pb.OrderManagement_ProcessOrdersClient, c chan struct{}) {
+	for {
+		combinedShipment, errProcOrder := streamProcOrder.Recv()
+		if errProcOrder == io.EOF {
+			break
+		}
+		log.Printf("Combined shipment : ", combinedShipment.OrdersList)
+	}
+	<-c
+}
